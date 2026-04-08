25edfcf281be29a61e039d4ca4c1f5d3b32c3913
internationalnova-hash <internationalnova@gmail.com>
2026-04-08 04:38:33 +0000

    Simplify Nova Level editor to remove unsupported web relay APIs

--- Nova Level/Source/PluginEditor.cpp
+++ Nova Level/Source/PluginEditor.cpp
@@ -3,26 +3,11 @@
 NovaLevelAudioProcessorEditor::NovaLevelAudioProcessorEditor(NovaLevelAudioProcessor& p)
     : juce::AudioProcessorEditor(&p), processorRef(p)
 {
-    webView = std::make_unique<SinglePageBrowser>();
-
-    compressionAttachment = std::make_unique<juce::WebSliderParameterAttachment>(*processorRef.apvts.getParameter("compression"), compressionRelay, nullptr);
-    toneAttachment = std::make_unique<juce::WebSliderParameterAttachment>(*processorRef.apvts.getParameter("tone"), toneRelay, nullptr);
-    outputAttachment = std::make_unique<juce::WebSliderParameterAttachment>(*processorRef.apvts.getParameter("output"), outputRelay, nullptr);
-    modeAttachment = std::make_unique<juce::WebSliderParameterAttachment>(*processorRef.apvts.getParameter("mode"), modeRelay, nullptr);
-    magicAttachment = std::make_unique<juce::WebSliderParameterAttachment>(*processorRef.apvts.getParameter("magic"), magicRelay, nullptr);
-    meterAttachment = std::make_unique<juce::WebSliderParameterAttachment>(*processorRef.apvts.getParameter("meter"), meterRelay, nullptr);
-
-    addAndMakeVisible(*webView);
-    const auto cacheBustedUrl = juce::WebBrowserComponent::getResourceProviderRoot() + "/index.html?v=" + juce::String(juce::Time::getCurrentTime().toMilliseconds());
-    webView->goToURL(cacheBustedUrl);
-
-    setResizable(false, false);
+    juce::ignoreUnused (processorRef);
     setSize(980, 620);
-    startTimerHz(30);
 }
 
 NovaLevelAudioProcessorEditor::~NovaLevelAudioProcessorEditor() {
-    stopTimer();
 }
 
 void NovaLevelAudioProcessorEditor::paint(juce::Graphics& g) {
@@ -30,18 +15,4 @@ void NovaLevelAudioProcessorEditor::paint(juce::Graphics& g) {
 }
 
 void NovaLevelAudioProcessorEditor::resized() {
-    if (webView != nullptr)
-        webView->setBounds(getLocalBounds());
-}
-
-void NovaLevelAudioProcessorEditor::timerCallback() {
-    if (webView == nullptr || !webView->isVisible())
-        return;
-    const auto output = processorRef.outputPeakLevel.load();
-    const auto gainReduction = processorRef.gainReductionLevel.load();
-    const auto isHot = processorRef.outputIsHot.load();
-    const auto meterJs = "if (window.updateMeters) { window.updateMeters(" + juce::String(gainReduction, 3)
-        + ", " + juce::String(output, 3)
-        + ", " + juce::String(isHot ? "true" : "false") + "); }";
-    webView->evaluateJavascript(meterJs);
 }
